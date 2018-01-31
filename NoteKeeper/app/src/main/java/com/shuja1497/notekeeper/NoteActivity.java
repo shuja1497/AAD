c07c68ab00c78ae9012cf08146393380d225075a
shuja1497 <shujareshi1497@gmail.com>
2018-01-31 20:03:10 +0530

    displaying contents in next activity

--- NoteKeeper/app/src/main/java/com/shuja1497/notekeeper/NoteActivity.java
+++ NoteKeeper/app/src/main/java/com/shuja1497/notekeeper/NoteActivity.java
@@ -1,5 +1,6 @@
 package com.shuja1497.notekeeper;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
@@ -17,6 +19,7 @@ import java.util.List;
 public class NoteActivity extends AppCompatActivity {
 
     public static final String NOTE_INFO = "com.shuja1497.notekeeper.NOTE_INFO";
+    private NoteInfo noteInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +34,29 @@ public class NoteActivity extends AppCompatActivity {
         adapter_courses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner_courses.setAdapter(adapter_courses);
 
+        readDisplayStateValues();
 
+        EditText textNoteTitle = findViewById(R.id.editText_note_title);
+        EditText textNoteText = findViewById(R.id.editText_note_text);
+
+        DisplayNotes(spinner_courses, textNoteTitle, textNoteText);
+
+    }
+
+    private void DisplayNotes(Spinner spinner_courses, EditText textNoteTitle, EditText textNoteText) {
+
+        List <CourseInfo> courses = DataManager.getInstance().getCourses();
+        int courseId = courses.indexOf(noteInfo.getCourse());
+
+        spinner_courses.setSelection(courseId);
+        textNoteTitle.setText(noteInfo.getTitle());
+        textNoteText.setText(noteInfo.getText());
+
+    }
+
+    private void readDisplayStateValues() {
+        Intent intent = getIntent();
+        noteInfo = intent.getParcelableExtra(NOTE_INFO);
 
     }
 
