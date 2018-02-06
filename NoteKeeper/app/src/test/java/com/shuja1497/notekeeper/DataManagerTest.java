9d56df40683c1245364c443192bee3abff9294f4
shuja1497 <shujareshi1497@gmail.com>
2018-02-06 20:11:47 +0530

    making tests more consistent using @BeforeClass

--- NoteKeeper/app/src/test/java/com/shuja1497/notekeeper/DataManagerTest.java
+++ NoteKeeper/app/src/test/java/com/shuja1497/notekeeper/DataManagerTest.java
@@ -3,6 +3,7 @@ package com.shuja1497.notekeeper;
 import android.provider.ContactsContract;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,29 +13,34 @@ import static org.junit.Assert.*;
  */
 public class DataManagerTest {
 
+    static DataManager sDataManager;
+
+    @BeforeClass
+    public static void ClassSetup() throws Exception{
+        sDataManager = DataManager.getInstance();
+    }
+
     @Before
     public void setUp() throws Exception{
-        DataManager dm = DataManager.getInstance();
-        dm.getNotes().clear();// each time the test runs i have a note list which is completely empty
-        dm.initializeExampleNotes();// each test now starts with a consistent set of notes each time.
+        sDataManager.getNotes().clear();// each time the test runs i have a note list which is completely empty
+        sDataManager.initializeExampleNotes();// each test now starts with a consistent set of notes each time.
     }
     @Test
     public void createNewNote() throws Exception {
         // for creating new note ..requirements --> course, note title, note text
 
-        DataManager dm = DataManager.getInstance();
-        final CourseInfo courseInfo = dm.getCourse("android_async");
+        final CourseInfo courseInfo = sDataManager.getCourse("android_async");
         final String noteTitle  = "Test note title";
         final String noteText = "Test note text";
 
-        int noteIndex = dm.createNewNote();
-        NoteInfo newNote = dm.getNotes().get(noteIndex);
+        int noteIndex = sDataManager.createNewNote();
+        NoteInfo newNote = sDataManager.getNotes().get(noteIndex);
 
         newNote.setCourse(courseInfo);
         newNote.setTitle(noteTitle);
         newNote.setText(noteText);
 
-        NoteInfo compareNote = dm.getNotes().get(noteIndex);
+        NoteInfo compareNote = sDataManager.getNotes().get(noteIndex);
 
         //Assert class ia static import so we can just directly use the mehtods of that class.
 //        assertSame(newNote, compareNote); //checks if two references point to same objects
@@ -48,29 +54,28 @@ public class DataManagerTest {
     @Test
     public void findSimilarNotes() throws Exception{
 
-        DataManager dm = DataManager.getInstance();
 
-        final CourseInfo course  = dm.getCourse("android_async");
+        final CourseInfo course  = sDataManager.getCourse("android_async");
         final String noteTitle = "Title for test2";
         final String noteText1 = "Text1 for test2";
         final String noteText2 = "Text2 for test2";
 
-        int NoteIndex1 = dm.createNewNote();
-        NoteInfo newNote1 = dm.getNotes().get(NoteIndex1);
+        int NoteIndex1 = sDataManager.createNewNote();
+        NoteInfo newNote1 = sDataManager.getNotes().get(NoteIndex1);
         newNote1.setCourse(course);
         newNote1.setTitle(noteTitle);
         newNote1.setText(noteText1);
 
-        int NoteIndex2 = dm.createNewNote();
-        NoteInfo newNote2 = dm.getNotes().get(NoteIndex2);
+        int NoteIndex2 = sDataManager.createNewNote();
+        NoteInfo newNote2 = sDataManager.getNotes().get(NoteIndex2);
         newNote2.setCourse(course);
         newNote2.setTitle(noteTitle);
         newNote2.setText(noteText2);
 
-        int foundIndex1 = dm.findNote(newNote1);
+        int foundIndex1 = sDataManager.findNote(newNote1);
         assertEquals(NoteIndex1, foundIndex1);
 
-        int foundIndex2 = dm.findNote(newNote2);
+        int foundIndex2 = sDataManager.findNote(newNote2);
         assertEquals(NoteIndex2, foundIndex2);
 
 
