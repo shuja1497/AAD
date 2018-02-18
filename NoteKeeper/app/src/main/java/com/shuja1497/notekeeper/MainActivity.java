92549d574cb5798922e126326a86f7bfa99f0863
shuja1497 <shujareshi1497@gmail.com>
2018-02-18 12:44:27 +0530

    setting up the launcher navDrawer screen and adding recyclerview

--- NoteKeeper/app/src/main/java/com/shuja1497/notekeeper/MainActivity.java
+++ NoteKeeper/app/src/main/java/com/shuja1497/notekeeper/MainActivity.java
@@ -1,8 +1,11 @@
 package com.shuja1497.notekeeper;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,8 +16,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private NoteRecyclerAdapter mNoteRecyclerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +34,8 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(MainActivity.this, NoteActivity.class));
+
             }
         });
 
@@ -40,8 +47,27 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-    }
 
+        initializeNoteList();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+//        adapter.notifyDataSetChanged();
+        mNoteRecyclerAdapter.notifyDataSetChanged(); // notifying adapter with any possible changes whenever we return to this activity .
+    }
+    private void initializeNoteList() {
+        final RecyclerView recyclerNotes = findViewById(R.id.list_items);
+        final LinearLayoutManager notesLinearlayoutManager = new LinearLayoutManager(this);
+
+        recyclerNotes.setLayoutManager(notesLinearlayoutManager);
+
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+        mNoteRecyclerAdapter = new NoteRecyclerAdapter(this, notes);
+
+        recyclerNotes.setAdapter(mNoteRecyclerAdapter);
+        //updating noterecyler adpater when something is changed
+    }
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
