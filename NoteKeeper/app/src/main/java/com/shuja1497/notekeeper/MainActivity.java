dd89469da78fbf61d54a963123778c17bd388a9d
shuja1497 <shujareshi1497@gmail.com>
2018-02-19 20:22:04 +0530

    working with values resources

--- NoteKeeper/app/src/main/java/com/shuja1497/notekeeper/MainActivity.java
+++ NoteKeeper/app/src/main/java/com/shuja1497/notekeeper/MainActivity.java
@@ -65,7 +65,7 @@ public class MainActivity extends AppCompatActivity
         mRecyclerItems = findViewById(R.id.list_items);
         mNotesLinearlayoutManager = new LinearLayoutManager(this);
 
-        mCoursesLayoutManager = new GridLayoutManager(this, 2);
+        mCoursesLayoutManager = new GridLayoutManager(this, getResources().getInteger(R.integer.course_grid_span));
 
         List<NoteInfo> notes = DataManager.getInstance().getNotes();
         mNoteRecyclerAdapter = new NoteRecyclerAdapter(this, notes);
