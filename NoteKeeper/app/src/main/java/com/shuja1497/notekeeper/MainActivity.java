dcb5b01c769d61a9c0392b9ddcc6b72d89143c0d
shuja1497 <shujareshi1497@gmail.com>
2018-02-20 14:30:21 +0530

    adding setting screen

--- NoteKeeper/app/src/main/java/com/shuja1497/notekeeper/MainActivity.java
+++ NoteKeeper/app/src/main/java/com/shuja1497/notekeeper/MainActivity.java
@@ -125,6 +125,8 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
