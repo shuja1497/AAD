bdf73b30d6045e7f357321dade0ff5345dc198fc
shuja1497 <shujareshi1497@gmail.com>
2018-02-21 20:57:14 +0530

    implementing Database OpenHelper class

---
+++ NoteKeeper/app/src/main/java/com/shuja1497/notekeeper/NoteKeeperOpenHelper.java
@@ -0,0 +1,37 @@
+package com.shuja1497.notekeeper;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by shuja1497 on 2/21/18.
+ */
+
+public class NoteKeeperOpenHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "NoteKeeper.db";
+    public static final int DATABASE_VERSION = 1;
+
+    public NoteKeeperOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL(NotekeeperDatabaseContract.CourseInfoEntry.SQL_CREATE_TABLE);
+        db.execSQL(NotekeeperDatabaseContract.NoteInfoEntry.SQL_CREATE_TABLE);
+
+        DatabaseDataWorker worker = new DatabaseDataWorker(db);
+
+        worker.insertCourses();
+        worker.insertSampleNotes();
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
