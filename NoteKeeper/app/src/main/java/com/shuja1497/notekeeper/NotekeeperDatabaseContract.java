aca653ec85edd4b0f2e58448eedc882f5cc91f5c
shuja1497 <shujareshi1497@gmail.com>
2018-02-28 00:01:55 +0530

    creating indexes in DB and upgrading the DB verisons

--- NoteKeeper/app/src/main/java/com/shuja1497/notekeeper/NotekeeperDatabaseContract.java
+++ NoteKeeper/app/src/main/java/com/shuja1497/notekeeper/NotekeeperDatabaseContract.java
@@ -19,6 +19,14 @@ public final class NotekeeperDatabaseContract {
         public static final String COLUMN_COURSE_ID = "course_id";
         public static final String COLUMN_COURSE_TITLE = "course_title";
 
+        // CREATE INDEX course_info_index_1 ON course_info (coursE_title, col2, ...)
+        // in case we want indexing to provide uniqueness ... CREATE UNIQUE INDEX
+
+        public static final String INDEX1 = TABLE_NAME+"_index1";
+        public static final String SQL_CREATE_INDEX1 =
+                "CREATE INDEX " + INDEX1 + " ON " + TABLE_NAME +
+                        "(" + COLUMN_COURSE_TITLE + ")";
+
         public static final String SQL_CREATE_TABLE =
                 "CREATE TABLE "+ TABLE_NAME + " (" +
                         _ID + " INTEGER PRIMARY KEY, " +
@@ -38,6 +46,10 @@ public final class NotekeeperDatabaseContract {
         public static final String COLUMN_NOTE_TITLE = "note_title";
         public static final String COLUMN_COURSE_ID = "course_id";
 
+        public static final String INDEX1 = TABLE_NAME+"_index1";
+        public static final String SQL_CREATE_INDEX1 =
+                "CREATE INDEX " + INDEX1 + " ON " + TABLE_NAME +
+                        "(" + COLUMN_NOTE_TITLE + ")";
 
         public static final String SQL_CREATE_TABLE =
                 "CREATE TABLE "+ TABLE_NAME + " (" +
