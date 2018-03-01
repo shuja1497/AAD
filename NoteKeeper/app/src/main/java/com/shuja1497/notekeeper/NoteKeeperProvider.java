0bb04f212ab269dca5d7d69e29f308fa38e0d9b6
shuja1497 <shujareshi1497@gmail.com>
2018-03-01 17:57:26 +0530

    implementing Uri handling using UriMatcher

--- NoteKeeper/app/src/main/java/com/shuja1497/notekeeper/NoteKeeperProvider.java
+++ NoteKeeper/app/src/main/java/com/shuja1497/notekeeper/NoteKeeperProvider.java
@@ -7,13 +7,32 @@ package com.shuja1497.notekeeper;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 
+import com.shuja1497.notekeeper.NoteKeeperProviderContract.Courses;
+import com.shuja1497.notekeeper.NoteKeeperProviderContract.Notes;
+
 public class NoteKeeperProvider extends ContentProvider {
 
     private NoteKeeperOpenHelper mDbOpenHelper;
+
+    // adding UriMatcher field
+    private static UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    public static final int COURSE = 0;
+
+    public static final int NOTES = 1;
+
+    // adding list of valid Uris using static initializer
+    static {
+        sUriMatcher.addURI(NoteKeeperProviderContract.AUTHORITY, Courses.PATH, COURSE);
+        sUriMatcher.addURI(NoteKeeperProviderContract.AUTHORITY, Notes.PATH, NOTES);
+
+        // after this we can handle Uris in query method
+    }
 
     public NoteKeeperProvider() {
     }
@@ -50,8 +69,18 @@ public class NoteKeeperProvider extends ContentProvider {
 
         Cursor cursor = null;
         SQLiteDatabase db  = mDbOpenHelper.getReadableDatabase();
-        cursor = db.query(NotekeeperDatabaseContract.CourseInfoEntry.TABLE_NAME, projection,
-                selection, selectionArgs, null, null, sortOrder);
+
+        int uriMatch = sUriMatcher.match(uri);//will return appropraite integar value for the matched uri
+        switch (uriMatch){
+            case COURSE:
+                cursor = db.query(NotekeeperDatabaseContract.CourseInfoEntry.TABLE_NAME, projection,
+                        selection, selectionArgs, null, null, sortOrder);
+                break;
+            case NOTES:
+                cursor = db.query(NotekeeperDatabaseContract.NoteInfoEntry.TABLE_NAME, projection,
+                        selection, selectionArgs, null, null, sortOrder);
+                break;
+        }
         return cursor;
     }
 
