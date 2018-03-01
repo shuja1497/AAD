b4020f5bbd00bb75c399c579544bea35f3a6aa37
shuja1497 <shujareshi1497@gmail.com>
2018-03-01 16:58:14 +0530

    adding table columns to Contract Class

--- NoteKeeper/app/src/main/java/com/shuja1497/notekeeper/NoteKeeperProviderContract.java
+++ NoteKeeper/app/src/main/java/com/shuja1497/notekeeper/NoteKeeperProviderContract.java
@@ -1,6 +1,7 @@
 package com.shuja1497.notekeeper;
 
 import android.net.Uri;
+import android.provider.BaseColumns;
 
 /**
  * Created by shuja1497 on 3/1/18.
@@ -16,21 +17,34 @@ public final class NoteKeeperProviderContract {
 
     // making private constructor so that no other can make instance of this class .
     private NoteKeeperProviderContract(){}
-
     public static final String AUTHORITY = "com.shuja1497.notekeeper.provider";
     public static final Uri AUTHORITY_URI = Uri.parse("content://" + AUTHORITY);// used to create specific uri's for individual tables
+
+    protected interface CoursesIdColumns{
+        // to remove redundancy
+        public static final String COLUMN_COURSE_ID = "course_id";
+    }
+
+    protected interface CoursesColumns{
+        public static final String COLUMN_COURSE_TITLE = "course_title";
+    }
+
+    protected interface NotesColumns{
+        public static final String COLUMN_NOTE_TITLE = "note_title";
+        public static final String COLUMN_NOTE_TEXT = "note_text";
+    }
 
     // creating nested classes for each of the table that we expose with this content provider
 
     // courses Table
-    public static final class Courses {
+    public static final class Courses implements BaseColumns, CoursesColumns, CoursesIdColumns{
         public static final String PATH = "courses";
         //"content://com.shuja1497.notekeeper.provider/courses"
         public static final Uri CONTENT_URI = Uri.withAppendedPath(AUTHORITY_URI, PATH);
     }
 
     // notes Table
-    public static final class Notes{
+    public static final class Notes implements BaseColumns, NotesColumns, CoursesIdColumns{
         public static final String PATH = "notes";
         public static final Uri CONTENT_URI = Uri.withAppendedPath(AUTHORITY_URI, PATH);
     }
