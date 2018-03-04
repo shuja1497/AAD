71580bb1cc201c6fcbe9d6d17f63c5f372c5d6b2
shuja1497 <shujareshi1497@gmail.com>
2018-03-04 16:48:50 +0530

    creating a job implementation class

---
+++ NoteKeeper/app/src/main/java/com/shuja1497/notekeeper/NoteUploaderJobService.java
@@ -0,0 +1,23 @@
+package com.shuja1497.notekeeper;
+
+import android.app.Service;
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.content.Intent;
+import android.os.IBinder;
+// we need to have a permission for this to act as a job service class ... in manifest
+public class NoteUploaderJobService extends JobService {
+    public NoteUploaderJobService() {
+    }
+
+    @Override
+    public boolean onStartJob(JobParameters params) {
+        return false;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters params) {
+        return false;
+    }
+
+}
