b8125a71d5274c8570fc5edd62e0488c51a20dec
shuja1497 <shujareshi1497@gmail.com>
2018-03-05 23:54:00 +0530

    fixing

--- NoteKeeper/app/src/main/java/com/shuja1497/notekeeper/NoteActivity.java
+++ NoteKeeper/app/src/main/java/com/shuja1497/notekeeper/NoteActivity.java
@@ -47,7 +47,7 @@ public class NoteActivity extends AppCompatActivity
     public static final String ORIGINAL_NOTE_TEXT = "com.shuja1497.notekeeper.ORIGINAL_NOTE_TEXT";
     public static final int LOADER_NOTES = 0;
     public static final int LOADER_COURSES = 1;
-    public static final String NOTE_URI;
+    public static final String NOTE_URI= "NOTE_URI";
     private final String TAG = getClass().getSimpleName();
     private NoteInfo mNote = new NoteInfo(DataManager.getInstance().getCourses().get(0), "", "");
     private boolean isNewNote;
